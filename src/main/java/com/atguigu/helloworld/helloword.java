8b01350f0ea43df326e5731c32edb3eca5fc4b90
rtz <xieyuxi@atguigu.com>
2020-10-09 23:24:03 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/helloworld/helloword.java
@@ -0,0 +1,14 @@
+package com.atguigu.helloworld;
+
+/**
+ * @Package: com.atguigu.helloworld
+ * @ClassName: helloword
+ * @Author: 恋空
+ * @CreateTime: 2020/10/9 21:25
+ * @Description:
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
